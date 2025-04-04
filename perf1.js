84cda07b73c5fb2f112c92fc3dba9df417e1b929
van015 <ulatvanremlee@gmail.com>
2025-04-04 14:52:10 +0800

    Add files via upload

---
+++ perf1.js
@@ -0,0 +1,12 @@
+function Number() {
+  var myVar = 25;
+  if (true) {
+    let myLet = 50;
+    const myConst = 10;
+    console.log(myVar);
+    console.log(myLet);
+    console.log(myConst);
+  }
+  console.log(myVar);
+}
+Number();
